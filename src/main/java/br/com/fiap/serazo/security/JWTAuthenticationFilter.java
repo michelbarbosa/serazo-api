40821661fa8d24bd3617b08b34e1ee6f66750775
Michel Barbosa <michel.barbosa@way2.com.br>
2017-09-09 12:02:09 -0300

    PARCIAL - implementando autenticacao JWT

---
+++ src/main/java/br/com/fiap/serazo/security/JWTAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package br.com.fiap.serazo.security;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.fiap.serazo.model.Empresa;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static br.com.fiap.serazo.security.SecurityConstants.EXPIRATION_TIME;
+import static br.com.fiap.serazo.security.SecurityConstants.HEADER_STRING;
+import static br.com.fiap.serazo.security.SecurityConstants.SECRET;
+import static br.com.fiap.serazo.security.SecurityConstants.TOKEN_PREFIX;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+        try {
+            Empresa creds = new ObjectMapper()
+                    .readValue(req.getInputStream(), Empresa.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getLogin(),
+                            creds.getSenha(),
+                            new ArrayList<>())
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        String token = Jwts.builder()
+                .setSubject(((User) auth.getPrincipal()).getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+        res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+    }
+}
